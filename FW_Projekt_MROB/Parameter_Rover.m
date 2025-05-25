87933c2bc7e4a2f0a93a8ed5b7fd2c0aa12e0dc8
thiago-643 <thiago.thielecke@gmx.de>
2025-05-25 14:40:37 +0200

    Update Parameter_Rover.m

--- FW_Projekt_MROB/Parameter_Rover.m
+++ FW_Projekt_MROB/Parameter_Rover.m
@@ -1,16 +1,25 @@
-%Initialisierung Parameter%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Initialisierung der Rover-Parameter%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-b = 3;                  % Breite    
-l = 2;                  % Länge
-h = 5;                  % Höhe
+% Geometrische Parameter des Rovers
+roverl = 0.5;          % Länge [m]
+roverb = 0.3;          % Breite [m]
+roverh = 0.2;          % Höhe [m]
+x = 0;
+y = 0;
+psi = 0;
+rover_radr = 0.05;     % Radradius [m]
 
-r1 = 4;                 % Radius Rad 1
-r2 = 4;                 % Radius Rad 2
-r3 = 4;                 % Radius Rad 3
-r4 = 4;                 % Radius Rad 4
 
-h1, h2, h3;             % 3 Hindernisse
-stp, enp;               % Startpunkt, Endpunkt
+% theta = 3;              %Anfangswinkel [rad/s]
+v = 0;                  %Anfangsgeschwindigkeit [m/s]
 
-v = 30;                 %Geschwindigkeit
-x = 10;
+sp = [0,0];             %Startpunkt [x y]   
+zp = [5,5];             %Zielpunkt  [x y]
+
+hindernisse = [
+    2,2,1.3
+    3,4,0.7
+    4,4,0.5 ];          %Hindernisse als Kreise [x y radius]
+
+dt = 0.01;               %Zeitintervall [s]
+% sim_time = 20;          %Simulationsdauer [s]    
