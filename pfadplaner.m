d7d0526ec60e7f98146353e7ea146d9ec6886e9e
thiago-643 <thiago.thielecke@gmx.de>
2025-06-18 16:32:46 +0200

    Add files via upload

---
+++ pfadplaner.m
@@ -0,0 +1,57 @@
+function [x_pfad, y_pfad] = pfadplaner(sp, zp, hindernisse,roverb)
+    % Eingabe:
+    % sp: Startpunkt [x, y]
+    % zp: Zielpunkt [x, y]
+    % hindernisse: Nx3 [x, y, r] Kreise
+    
+    % Parameter
+    schrittweite = 0.2;                %Genauigkeit
+    max_iter = 1000;
+    sicherheitsabstand = roverb + 0.23;
+
+    % Initialisierung
+    pos = sp;
+    ziel = zp;
+    x_pfad = pos(1);
+    y_pfad = pos(2);
+
+    for k = 1:max_iter
+        richtung = (ziel - pos) / norm(ziel - pos);     % Richtung als Zeilen-Einheitsvektor
+        neue_pos = pos + schrittweite * richtung;       % ein schritt in Zielrichtung
+
+        % Kollision prüfen
+        kollidiert = false;
+        for i = 1:size(hindernisse, 1)
+            hx = hindernisse(i,1);
+            hy = hindernisse(i,2);
+            hr = hindernisse(i,3) + sicherheitsabstand;
+            if norm(neue_pos - [hx, hy]) < hr
+                kollidiert = true;
+                break;
+            end
+        end
+
+        if kollidiert
+            % ausweichen um 90° 
+            winkel = atan2(richtung(2), richtung(1)) + pi/2;
+            umgehung = [cos(winkel), sin(winkel)];
+            neue_pos = pos + schrittweite * umgehung;    %Ein schritt in ausweichrichtung
+        end
+
+        % Speichern und weitermachen
+        pos = neue_pos;
+        x_pfad(end+1) = pos(1);
+        y_pfad(end+1) = pos(2);
+
+        % Abbruchbedingung
+        if norm(pos - ziel) < schrittweite
+            break;
+        end
+    end
+
+    %Ziel hinzufügen
+    x_pfad(end+1) = ziel(1);
+    y_pfad(end+1) = ziel(2);
+     
+
+end
---
+++ update_raeder.m
@@ -0,0 +1,45 @@
+function update_raeder(phi,roverl,roverb,rad_l,rad_b)
+    persistent radHandles bodyHandle
+    % Positionen der vier Räder relativ zum Roverzentrum
+    rad_pos = [
+         roverl/2,  roverb/2;
+         roverl/2, -roverb/2;
+        -roverl/2, -roverb/2;
+        -roverl/2,  roverb/2
+    ];
+
+    if isempty(radHandles) || ~all(isvalid(radHandles)) || isempty(bodyHandle) || ~isvalid(bodyHandle)
+        figure(2); 
+        clf;
+        set(gcf, 'Name', 'Rover Draufsicht');
+        axis equal; 
+        xlim([-0.5 0.5]); ylim([-0.5 0.5]);
+        grid on; 
+        title('Rover Draufsicht'); 
+        hold on;
+
+        % --- Roverkörper als Rechteck ---
+        % Basisrechteck zentriert um Ursprung
+        body_corners = 0.5 * [
+            -roverl, -roverb;
+             roverl, -roverb;
+             roverl,  roverb;
+            -roverl,  roverb
+        ];
+        bodyHandle = fill(body_corners(:,1), body_corners(:,2), [0.7 0.7 0.7], ...
+                          'EdgeColor', 'k', 'LineWidth', 1.5, 'FaceAlpha', 0.8);
+
+        % --- Räder zeichnen ---
+        radHandles = gobjects(4,1);
+        for i = 1:4
+            corners = rotate_rect(rad_l, rad_b, phi(i)) + rad_pos(i,:);
+            radHandles(i) = fill(corners(:,1), corners(:,2), [0.2 0.6 1], 'EdgeColor', 'k');
+        end
+    else
+        % --- Räder aktualisieren ---
+        for i = 1:4
+            corners = rotate_rect(rad_l, rad_b, phi(i)) + rad_pos(i,:);
+            set(radHandles(i), 'XData', corners(:,1), 'YData', corners(:,2));
+        end
+    end
+end
