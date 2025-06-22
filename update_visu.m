c014d125fdba74f7d87af769ec1067f36e944e75
melisa <120559871+melisa2910@users.noreply.github.com>
2025-06-22 14:07:24 +0200

    Delete update_visu.m

--- update_visu.m
+++
@@ -1,5 +0,0 @@
-function update_visu(X_sp, Y_sp,x, y, psi, phi,roverl,roverb,rad_l,rad_b)
-    coder.extrinsic('roverplot', 'rovertopview')
-    roverplot(X_sp, Y_sp,x, y, psi,roverl,roverb);
-    update_raeder(phi,roverl,roverb,rad_l,rad_b);
-end
